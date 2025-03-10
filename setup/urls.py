b3accab0fe757905e31ae97dd2561cbec8702ac8
Gabriel T <gabbsenai0@gmail.com>
2025-03-10 14:41:20 -0300

    v02

--- setup/urls.py
+++ setup/urls.py
@@ -17,7 +17,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api_ecommerce.api import viewsets
-from rest_framework import routers
+from rest_framework import routers, permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Ecommerce API",
+        default_version='v1',
+        description="Sistema para controle do ecommerce",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contato@doador.com.br"),
+        license=openapi.License(name="Free"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 
 route = routers.DefaultRouter()
 route.register(r'categoria', viewsets.CategoriaViewSet)
@@ -29,4 +45,9 @@ route.register(r'recibo', viewsets.ReciboViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls)),
+]
+urlpatterns += [
+    path('swaggerjson', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
