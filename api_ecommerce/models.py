abcad004a6db3318dc02d2cd02cc93ed6cdca0d4
João Vitor Santiago <jv.santiago.souza@gmail.com>
2025-04-08 10:52:13 -0300

    v3.6

--- api_ecommerce/models.py
+++ api_ecommerce/models.py
@@ -11,10 +11,11 @@ class Produto(models.Model):
     nome = models.CharField(max_length=50)
     descricao = models.TextField(max_length=255)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    vendedor = models.ForeignKey('Vendedor', on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.nome
-    
+
 class Vendedor(models.Model):
     nome = models.CharField(max_length=50)
     email = models.EmailField()
@@ -25,7 +26,7 @@ class Vendedor(models.Model):
 
     def __str__(self):
         return self.nome
-    
+
 class Anuncio(models.Model):
     produto = models.ForeignKey(Produto, on_delete=models.DO_NOTHING)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
@@ -45,7 +46,7 @@ class Anuncio(models.Model):
 
     def __str__(self):
         return f"{self.produto.nome} - R$ {self.valor}"
-    
+
 class Recibo(models.Model):
     anuncio = models.ForeignKey(Anuncio, on_delete=models.DO_NOTHING)
     vendedor = models.ForeignKey(Vendedor, on_delete=models.DO_NOTHING)
@@ -54,3 +55,10 @@ class Recibo(models.Model):
 
     def __str__(self):
         return f"Recibo de {self.anuncio.produto.nome} - {self.data}"
+
+class Carrinho(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.DO_NOTHING)
+    quantidade = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.produto.nome} - Quantidade: {self.quantidade}"
